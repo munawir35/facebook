ed91b9f248b69ed5fb0ed724265a88f7d2d625c4
munawir4 <munawir4@gmail.com>
2021-09-30 12:00:36 +0530

    new

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::view('fb','first');
+Route::view('first','first');
 Route::view('login','login');
 Route::view('sign','sign');
